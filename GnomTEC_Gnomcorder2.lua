c538452613682e55289c5e3f653870d0ab96301e
GnomTEC <info@gnomtec.de>
2016-10-30 11:35:02 +0100

    * Updated TOC for WoW patch 7.1.0

--- GnomTEC_Gnomcorder2.lua
+++ GnomTEC_Gnomcorder2.lua
@@ -1,6 +1,6 @@
 -- **********************************************************************
 -- GnomTEC Gnomcorder2
--- Version: 7.0.3.1
+-- Version: 7.1.0.1
 -- Author: Peter Jack
 -- URL: http://www.gnomtec.de/
 -- **********************************************************************
@@ -28,8 +28,8 @@ local L = LibStub("AceLocale-3.0"):GetLocale("GnomTEC_Gnomcorder2")
 local addonInfo = {
 	["Name"] = "GnomTEC Gnomcorder2",
 	["Description"] = "GnomTEC Gnomcorder Series II.",	
-	["Version"] = "7.0.3.1",
-	["Date"] = "2016-07-20",
+	["Version"] = "7.1.0.1",
+	["Date"] = "2016-10-30",
 	["Author"] = "Peter Jack",
 	["Email"] = "info@gnomtec.de",
 	["Website"] = "http://www.gnomtec.de/",
